48a25cdc8f09d6d566369a594d9b6caf92c1f5f9
levarev <levonarev2@gmail.com>
2026-02-19 13:53:32 +0400

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
